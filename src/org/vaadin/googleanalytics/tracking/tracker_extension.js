112c85a48857d326d7e86947690a7ce09631e51e
Artur Signell <artur@vaadin.com>
2013-02-06 13:46:12 +0200

    Cleaned up logging

--- src/org/vaadin/googleanalytics/tracking/tracker_extension.js
+++ src/org/vaadin/googleanalytics/tracking/tracker_extension.js
@@ -7,22 +7,18 @@ window.org_vaadin_googleanalytics_tracking_GoogleAnalyticsTracker = function() {
 			var domainName = state.domainName;
 
 			try {
-
 				var pageTracker = window._gat._getTracker(trackerId);
-				console.log("Using tracker id " + trackerId);
 				if (domainName) {
-					console.log("Tracking domain " + domainName);
 					pageTracker._setDomainName(domainName);
 				}
 				if (pageId) {
 					pageTracker._trackPageview(pageId);
-					console.log("Tracked page " + pageId);
 				} else {
 					pageTracker._trackPageview();
-					console.log("Tracked unknown page");
 				}
 			} catch (err) {
-				window.alert(err);
+				if (console)
+					console.log(err);
 			}
 
 		}
