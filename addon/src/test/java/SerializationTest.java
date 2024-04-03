57ca092523768ce40bd491a90b961baa84ae05c0
Sami Ekblad <sami@vaadin.com>
2024-04-03 17:22:26 +0300

    Add simple Java serialization test.

---
+++ addon/src/test/java/SerializationTest.java
@@ -0,0 +1,54 @@
+/*-
+ * Serialization test.
+ *
+ * Credits and Copyright (C) 2023 Flowing Code
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import com.vaadin.flow.component.UI;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.vaadin.googleanalytics.tracking.GoogleAnalyticsTracker;
+
+public class SerializationTest {
+
+    private void testSerializationOf(Object obj) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject(obj);
+        }
+        try (ObjectInputStream in =
+                     new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()))) {
+            obj.getClass().cast(in.readObject());
+        }
+    }
+
+    @Test
+    public void testSerialization() throws ClassNotFoundException, IOException {
+        try (MockedStatic<UI> mockUI = Mockito.mockStatic(UI.class)) {
+            mockUI.when(UI::getCurrent).thenReturn(new UI());
+            testSerializationOf(GoogleAnalyticsTracker.getCurrent());
+        } catch (Exception e) {
+            Assert.fail("Problem while testing serialization: " + e.getMessage());
+        }
+    }
+}
